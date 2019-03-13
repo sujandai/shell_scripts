245cdde93708cdcf1d3a273493ab218493622f22
Sujan <kecy2975@gmail.com>
2019-03-13 05:20:40 +0000

    Basic Scripts

---
+++ sys_info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#system Information
+clear
+echo ""
+echo "The current user is: `whoami`" 
+echo ""
+echo "The hostname is: `hostname`"
+echo ""
+echo "Operating System is: `uname`"
+echo ""
+echo ""
---
+++ test_cmd2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#This script checks whether a variable has content
+
+my_variable="Full"
+
+if test $my_variable
+then
+   echo "The $my_variable returns a True"
+else
+   echo "The $my_variable returns a False"
+fi
